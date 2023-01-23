fb611561b7ac96c005a525ca9f99aaf8574e9ffb
elenaBezro <lenyka_86@mail.ru>
2023-01-24 00:07:39 +0100

    feat: catch errors

--- src/index.ts
+++ src/index.ts
@@ -46,108 +46,114 @@ wss.on("connection", function connection(ws) {
   //TODO: обработка ошибок?
 
   duplex.on("data", async (command: string) => {
-    console.log("received: %s", command);
+    try {
+      console.log("received: %s", command);
 
-    if (command.startsWith("mouse_up")) {
-      let commandText = command.split(" ")[0];
-      await mouse.move(up(+command.slice(commandText.length)));
-      const msgToSendBack = command.split(" ").join("");
-      duplex.write(msgToSendBack, (err) => {
-        if (err) {
-          console.error("Oops, something went wrong", err);
-        }
-        console.log(`send: ${command.slice(0, 8)} on${command.slice(8)} px \n`);
-      });
-    }
+      if (command.startsWith("mouse_up")) {
+        let commandText = command.split(" ")[0];
+        await mouse.move(up(+command.slice(commandText.length)));
+        const msgToSendBack = command.split(" ").join("");
+        duplex.write(msgToSendBack, (err) => {
+          if (err) {
+            console.error("Oops, something went wrong", err);
+          }
+          console.log(`send: ${command.slice(0, 8)} on${command.slice(8)} px \n`);
+        });
+      }
 
-    if (command.startsWith("mouse_down")) {
-      let commandText = command.split(" ")[0];
-      await mouse.move(down(+command.slice(commandText.length)));
-      const msgToSendBack = command.split(" ").join("");
-      duplex.write(msgToSendBack, (err) => {
-        if (err) {
-          console.error("Oops, something went wrong", err);
-        }
-        console.log(`send: ${command.slice(0, commandText.length)} on${command.slice(commandText.length)} px \n`);
-      });
-    }
+      if (command.startsWith("mouse_down")) {
+        let commandText = command.split(" ")[0];
+        await mouse.move(down(+command.slice(commandText.length)));
+        const msgToSendBack = command.split(" ").join("");
+        duplex.write(msgToSendBack, (err) => {
+          if (err) {
+            console.error("Oops, something went wrong", err);
+          }
+          console.log(`send: ${command.slice(0, commandText.length)} on${command.slice(commandText.length)} px \n`);
+        });
+      }
 
-    if (command.startsWith("mouse_left")) {
-      let commandText = command.split(" ")[0];
-      await mouse.move(left(+command.slice(commandText.length)));
-      const msgToSendBack = command.split(" ").join("");
-      duplex.write(msgToSendBack, (err) => {
-        if (err) {
-          console.error("Oops, something went wrong", err);
-        }
-        console.log(`send: ${command.slice(0, commandText.length)} on${command.slice(commandText.length)} px \n`);
-      });
-    }
+      if (command.startsWith("mouse_left")) {
+        let commandText = command.split(" ")[0];
+        await mouse.move(left(+command.slice(commandText.length)));
+        const msgToSendBack = command.split(" ").join("");
+        duplex.write(msgToSendBack, (err) => {
+          if (err) {
+            console.error("Oops, something went wrong", err);
+          }
+          console.log(`send: ${command.slice(0, commandText.length)} on${command.slice(commandText.length)} px \n`);
+        });
+      }
 
-    if (command.startsWith("mouse_right")) {
-      let commandText = command.split(" ")[0];
-      await mouse.move(right(+command.slice(commandText.length)));
-      const msgToSendBack = command.split(" ").join("");
-      duplex.write(msgToSendBack, (err) => {
-        if (err) {
-          console.error("Oops, something went wrong", err);
-        }
-        console.log(`send: ${command.slice(0, commandText.length)} on${command.slice(commandText.length)} px \n`);
-      });
-    }
+      if (command.startsWith("mouse_right")) {
+        let commandText = command.split(" ")[0];
+        await mouse.move(right(+command.slice(commandText.length)));
+        const msgToSendBack = command.split(" ").join("");
+        duplex.write(msgToSendBack, (err) => {
+          if (err) {
+            console.error("Oops, something went wrong", err);
+          }
+          console.log(`send: ${command.slice(0, commandText.length)} on${command.slice(commandText.length)} px \n`);
+        });
+      }
 
-    if (command.startsWith("mouse_position")) {
-      const { x, y } = await mouse.getPosition();
-      const msgToSendBack = `${command} ${x},${y}`;
+      if (command.startsWith("mouse_position")) {
+        const { x, y } = await mouse.getPosition();
+        const msgToSendBack = `${command} ${x},${y}`;
 
-      duplex.write(msgToSendBack, (err) => {
-        if (err) {
-          console.error("Oops, something went wrong", err);
-          // TODO: return ?
-        }
-        console.log(`send: ${msgToSendBack}\n`);
-      });
-    }
+        duplex.write(msgToSendBack, (err) => {
+          if (err) {
+            console.error("Oops, something went wrong", err);
+            // TODO: return ?
+          }
+          console.log(`send: ${msgToSendBack}\n`);
+        });
+      }
 
-    if (command.startsWith("draw_square")) {
-      const [commandText, squareSideStr] = command.split(" ");
-      const squareSide = +squareSideStr;
-      //TODO: read canvas's edge and draw only within the frame
-      await drawRectangle(squareSide, squareSide);
+      if (command.startsWith("draw_square")) {
+        const [commandText, squareSideStr] = command.split(" ");
+        const squareSide = +squareSideStr;
+        //TODO: read canvas's edge and draw only within the frame
+        await drawRectangle(squareSide, squareSide);
 
-      duplex.write(command, (err) => {
-        if (err) {
-          console.error("Oops, something went wrong", err);
-        }
-        console.log(`send: ${command}} \n`);
-      });
-    }
+        duplex.write(command, (err) => {
+          if (err) {
+            console.error("Oops, something went wrong", err);
+          }
+          console.log(`send: ${command}} \n`);
+        });
+      }
 
-    if (command.startsWith("draw_rectangle")) {
-      const [_commandText, firstSideStr, secondSideStr] = command.split(" ");
-      const firstSide = +firstSideStr;
-      const secondSide = +secondSideStr;
-      await drawRectangle(firstSide, secondSide);
+      if (command.startsWith("draw_rectangle")) {
+        const [_commandText, firstSideStr, secondSideStr] = command.split(" ");
+        const firstSide = +firstSideStr;
+        const secondSide = +secondSideStr;
+        await drawRectangle(firstSide, secondSide);
 
-      duplex.write(command, (err) => {
-        if (err) {
-          console.error("Oops, something went wrong", err);
-        }
-        console.log(`send: ${command}} \n`);
-      });
-    }
+        duplex.write(command, (err) => {
+          if (err) {
+            console.error("Oops, something went wrong", err);
+          }
+          console.log(`send: ${command}} \n`);
+        });
+      }
 
-    if (command.startsWith("draw_circle")) {
-      const [_commandText, radiusStr] = command.split(" ");
-      const radius = +radiusStr;
-      await drawCircle(radius);
+      if (command.startsWith("draw_circle")) {
+        const [_commandText, radiusStr] = command.split(" ");
+        const radius = +radiusStr;
+        await drawCircle(radius);
 
-      duplex.write(command, (error) => {
-        if (error) {
-          console.error("Oops, something went wrong", error);
-        }
-        console.log(`send: ${command}} \n`);
-      });
+        duplex.write(command, (error) => {
+          if (error) {
+            console.error("Oops, something went wrong", error);
+          }
+          console.log(`send: ${command}} \n`);
+        });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
     }
   });
 });
